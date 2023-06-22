85035aab4930ac150ef6e617fea1e812cd384718
Daegyeom Ha <combbm@gmail.com>
2023-06-22 21:23:49 +0900

    feat: USER_NOT_FOUND 오류 추가

--- src/main/java/leets/memoriessquare/global/error/ErrorCode.java
+++ src/main/java/leets/memoriessquare/global/error/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    USER_NOT_FOUND(404, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
     INVALID_REQUEST(400, "INVALID_REQUEST", "요청이 올바르지 않습니다."),
     COOKIE_NOT_FOUND(400, "COOKIE_NOT_FOUND", "쿠키를 찾을 수 없습니다."),
