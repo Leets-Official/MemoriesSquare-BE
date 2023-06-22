781a3f0213edc0895221d8c9f61a7b1c2409eb30
Daegyeom Ha <combbm@gmail.com>
2023-06-22 23:50:42 +0900

    feat: 소셜로그인 관련 설정 추가

--- src/main/java/leets/memoriessquare/global/security/WebSecurityConfig.java
+++ src/main/java/leets/memoriessquare/global/security/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package leets.memoriessquare.global.security;
 import leets.memoriessquare.global.filter.ExceptionHandleFilter;
 import leets.memoriessquare.global.jwt.JwtFilter;
 import leets.memoriessquare.global.jwt.JwtProvider;
+import leets.memoriessquare.global.oauth.OAuthDetailService;
+import leets.memoriessquare.global.oauth.OAuthSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,8 @@ import org.springframework.web.cors.CorsUtils;
 @EnableWebSecurity
 public class WebSecurityConfig {
     private final JwtProvider jwtProvider;
+    private final OAuthDetailService oAuthDetailService;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -41,6 +45,18 @@ public class WebSecurityConfig {
 
                 .anyRequest().authenticated()
                 .and()
+
+                .oauth2Login()
+                .successHandler(oAuthSuccessHandler)
+                .redirectionEndpoint()
+                .baseUri("/oauth/callback/**")
+                .and()
+                .authorizationEndpoint()
+                .baseUri("/oauth/login")
+                .and()
+                .userInfoEndpoint()
+                .userService(oAuthDetailService)
+                .and().and()
 
                 .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new ExceptionHandleFilter(), JwtFilter.class)
