bf99d2398cfc4b2b914fb689677f4e7132eebd46
Daegyeom Ha <combbm@gmail.com>
2023-06-23 15:11:30 +0900

    fix: 필요없는 설정 삭제

--- src/main/java/leets/memoriessquare/global/security/CorsConfig.java
+++ src/main/java/leets/memoriessquare/global/security/CorsConfig.java
@@ -3,11 +3,9 @@ package leets.memoriessquare.global.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${cors.origin.development}")
