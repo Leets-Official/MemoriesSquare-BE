0d0a9140f7cb30af92a00cbc795066afcf902f45
Daegyeom Ha <combbm@gmail.com>
2023-06-23 14:04:45 +0900

    feat: Spring Security 에러 핸들러 추가

--- src/main/java/leets/memoriessquare/global/security/WebSecurityConfig.java
+++ src/main/java/leets/memoriessquare/global/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package leets.memoriessquare.global.security;
 
 import leets.memoriessquare.global.filter.ExceptionHandleFilter;
+import leets.memoriessquare.global.jwt.AuthRole;
 import leets.memoriessquare.global.jwt.JwtFilter;
 import leets.memoriessquare.global.jwt.JwtProvider;
 import leets.memoriessquare.global.oauth.OAuthDetailService;
@@ -32,34 +33,43 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .cors().and()
                 .csrf().disable()
                 .formLogin().disable()
+                .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(401))
+                .accessDeniedHandler((request, response, accessDeniedException) -> response.sendError(403))
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers(CorsUtils::isCorsRequest).permitAll()
 
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
 
+                .requestMatchers("/user/me").hasAuthority(AuthRole.ROLE_USER.getRole())
+
                 .anyRequest().authenticated()
                 .and()
 
                 .oauth2Login()
-                .successHandler(oAuthSuccessHandler)
-                .redirectionEndpoint()
-                .baseUri("/oauth/callback/**")
-                .and()
                 .authorizationEndpoint()
                 .baseUri("/oauth/login")
                 .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth/callback/**")
+                .and()
                 .userInfoEndpoint()
                 .userService(oAuthDetailService)
-                .and().and()
+                .and()
+                .successHandler(oAuthSuccessHandler)
+                .and()
 
                 .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new ExceptionHandleFilter(), JwtFilter.class)
-                .build();
+                .addFilterBefore(new ExceptionHandleFilter(), JwtFilter.class);
+
+        return http.build();
     }
 }
