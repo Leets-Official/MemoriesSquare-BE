369fb9ab4f127cd5064adccd7638f133ee486c40
Daegyeom Ha <combbm@gmail.com>
2023-07-10 20:33:40 +0900

    feat: 구글 소셜로그인 구현

--- src/main/java/leets/memoriessquare/domain/user/type/Vendor.java
+++ src/main/java/leets/memoriessquare/domain/user/type/Vendor.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Vendor {
-    FACEBOOK("FACEBOOK");
+    FACEBOOK("FACEBOOK"),
+    GOOGLE("GOOGLE");
 
     private final String vendor;
 }
