3647d8031aeb4dc4b817ea056c21e17222be29f7
Daegyeom Ha <combbm@gmail.com>
2023-07-14 12:22:50 +0900

    HOTFIX: AWS S3 키 정보를 불러오도록 변경

--- src/main/java/leets/memoriessquare/domain/photo/usecase/S3Configuration.java
+++ src/main/java/leets/memoriessquare/domain/photo/usecase/S3Configuration.java
@@ -1,21 +1,30 @@
-//package leets.memoriessquare.infrastructure.config;
 package leets.memoriessquare.domain.photo.usecase;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Configuration {
-    @Value("${aws.region}") // application.properties에서 aws.region 값을 읽어옵니다.
+    @Value("${aws.region}")
     private String region;
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
 
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-                .region(Region.of(region)) // 리전을 지정합니다.
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
