4476039c811e31a0e209551ab4ca1ec6e1884c09
Daegyeom Ha <combbm@gmail.com>
2023-07-18 00:02:07 +0900

    feat: mapstruct 적용

--- src/main/java/leets/memoriessquare/domain/photo/usecase/GetPhotoByDateImpl.java
+++ src/main/java/leets/memoriessquare/domain/photo/usecase/GetPhotoByDateImpl.java
@@ -2,25 +2,27 @@ package leets.memoriessquare.domain.photo.usecase;
 
 import leets.memoriessquare.domain.photo.domain.Photo;
 import leets.memoriessquare.domain.photo.presentation.dto.PhotoWithDateDTO;
+import leets.memoriessquare.domain.photo.presentation.mapper.PhotoMapper;
 import leets.memoriessquare.domain.photo.repository.PhotoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 public class GetPhotoByDateImpl implements GetPhotoByDate {
     private final PhotoRepository photoRepository;
+    private final PhotoMapper photoMapper;
 
     @Override
     public List<PhotoWithDateDTO> execute(UUID userId, LocalDate date) {
-        List<Photo> photos = photoRepository.findAllByUser_IdAndCreatedAtBetweenOrderByCreatedAtDesc(userId, date.atTime(0, 0), date.atTime(23, 59));
-        return photos.stream().map(i ->
-                new PhotoWithDateDTO(i.getId(), i.getUser().getId(), i.getImageUrl(), i.isCrop(), Objects.requireNonNullElse(i.getOriginalPhoto().getId(), null), i.getCreatedAt(), i.getUpdatedAt())
-        ).toList();
+        List<Photo> photos = photoRepository
+                .findAllByUser_IdAndCreatedAtBetweenOrderByCreatedAtDesc(
+                        userId, date.atTime(0, 0), date.atTime(23, 59)
+                );
+        return photos.stream().map(photoMapper::mappingPhototoDto).toList();
     }
 }
